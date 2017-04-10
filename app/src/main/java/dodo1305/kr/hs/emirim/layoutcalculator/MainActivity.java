f493f346083aeb8712ac1ab6a48b9ee2982ed70b
dodo1305 <dodo1305@naver.com>
2017-04-10 20:58:47 +0900

    #2 Event Programming 2

--- app/src/main/java/dodo1305/kr/hs/emirim/layoutcalculator/MainActivity.java
+++ app/src/main/java/dodo1305/kr/hs/emirim/layoutcalculator/MainActivity.java
@@ -6,6 +6,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static dodo1305.kr.hs.emirim.layoutcalculator.R.id.text_result;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +21,7 @@ Button [] butnums = new Button[10];
     public static final int SELECT_EDIT2=1;
     int selectEdit=SELECT_EDIT1;
     String numStr="";
-
+    TextView textResult;
 
     /*int ids[]={R.id.but0,R.id.but1,R.id.but2,R.id.but3,R.id.but4,R.id.but5,R.id.but6,R.id.but7,R.id.but8,R.id.but9};*/
     @Override
@@ -27,6 +32,7 @@ Button [] butnums = new Button[10];
          edit1= (EditText)findViewById(R.id.edit2);
         edit1.setOnTouchListener(editHandler);
          edit2.setOnTouchListener(editHandler);
+        textResult=(TextView)findViewById(text_result);
 
         for(int i =0; i<butnums.length; i++) {
             butnums[i] = (Button) findViewById(R.id.but0 + i);
@@ -58,7 +64,26 @@ Button [] butnums = new Button[10];
     View.OnClickListener butOpHandler = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            EditText edit = (EditText)view;
+            int num1= Integer.parseInt(edit1.getText().toString()); //edit1에 입력받은 값
+            int num2= Integer.parseInt(edit1.getText().toString()); //edit2에 입력받은 값
+            double result=0;
+            switch (view.getId()){
+                case R.id.butop1:
+                    result=num1+num2;
+                    break;
+                case R.id.butop2:
+                    result=num1-num2;
+                    break;
+                case R.id.butop3:
+                    result=num1*num2;
+                    break;
+                case R.id.butop4:
+                    result=num1/(double)num2;
+                    break;
+            }//end of switch
+            textResult.setText("계산 결과 = "+result);
+
+
         }
     };
 
